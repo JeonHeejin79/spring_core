03f7473848e46b17d2d8007673ac3c2597cc7182
SKMNS\202110043 <jeonheejin@sk.com>
2023-08-03 10:57:11 +0900

    20230803

--- src/main/java/com/example/demo/lec06_eventpublisher/MyEventHandler.java
+++ src/main/java/com/example/demo/lec06_eventpublisher/MyEventHandler.java
@@ -1,16 +1,11 @@
 package com.example.demo.lec06_eventpublisher;
 
 import org.springframework.context.ApplicationListener;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyEventHandler implements ApplicationListener<MyEvent> {
     @Override
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    @Async
     public void onApplicationEvent(MyEvent event) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("이벤트를 받았다. 데이터는 " + event.getData());
--- src/main/java/com/example/demo/lec06_eventpublisher/MyEventHandlerAfter.java
+++ src/main/java/com/example/demo/lec06_eventpublisher/MyEventHandlerAfter.java
@@ -1,9 +1,10 @@
 package com.example.demo.lec06_eventpublisher;
 
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Component;
 public class MyEventHandlerAfter {
 
     @EventListener
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Async
     public void onApplicationEventAfter(MyEventAfter event) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("AFTER 이벤트를 받았다. 데이터는 " + event.getData());
